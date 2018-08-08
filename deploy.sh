59706833202bad254b77a70c75e1796cdd155e25
priyankachennupati <40577708+priyankachennupati@users.noreply.github.com>
2018-08-08 12:45:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 ENVIRONMENT=$1
 if [ $ENVIRONMENT = "QA" ]; then
 for i in `cat ip.txt`
-do
-  echo "Deploying war file into $i QA server.."
-  sleep 3
-  sshpass -p "priya1@" scp target/chennupati1.war priya@$i:/home/priya/apache-tomcat-8.5.32/webapps
-  echo "starting tomcat server in $i QA server.."
-sshpass -p "priya1@" ssh priya@$i "JAVA_HOME=/home/priya/jdk1.8.0_172" "/home/priya/apache-tomcat-8.5.32/bin/./startup.sh"
-done
+ do
+   echo "Deploying war file into $i QA server.."
+   sleep 3
+   sshpass -p "priya1@" scp target/chennupati1.war priya@$i:/home/priya/apache-tomcat-8.5.32/webapps
+   echo "starting tomcat server in $i QA server.."
+   sshpass -p "priya1@" ssh priya@$i "JAVA_HOME=/home/priya/jdk1.8.0_172" "/home/priya/apache-tomcat-8.5.32/bin/./startup.sh"
+ done
  echo "deploy success"
 fi
